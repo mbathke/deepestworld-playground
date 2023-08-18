36b208a31124c99b3208f8201646dfdeea113fcc
Michael Bathke <michael.bathke@avenga.com>
2023-08-19 00:50:43 +0200

    Added comment to describe what has to be done for communication.

--- src/starter.ts
+++ src/starter.ts
@@ -1,3 +1,5 @@
+// TODO: for communication with the websocket server we might have to go through 
+// the electron app, so this is currently not working.
 let url = new URL("/realtime/messages", window.location.href);
 url.protocol = url.protocol.replace("http", "ws");
 
